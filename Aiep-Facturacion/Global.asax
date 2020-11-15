63c45898e6a51e19d6e551686e3ebde41e0e46ed
Efra <egaray7@gmail.com>
2020-11-14 22:12:47 -0300

    fix(): add Global asx

---
+++ Aiep-Facturacion/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Inherits="Aiep_Facturacion.Global" %>
---
+++ Aiep-Facturacion/Global.asax.cs
@@ -0,0 +1,11 @@
+﻿using System.Web;
+
+namespace Aiep_Facturacion
+{
+    public class Global : HttpApplication
+    {
+        protected void Application_Start()
+        {
+        }
+    }
+}
