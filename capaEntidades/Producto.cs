07906e929d6c0aa70e1a79f6189a1af81fac9da1
Efrain Garay <egaray7@gmail.com>
2020-11-18 22:18:32 -0300

    id producto

--- capaEntidades/Producto.cs
+++ capaEntidades/Producto.cs
@@ -29,7 +29,7 @@ namespace CapaEntidades
         public int Stock { get => stock; set => stock = value; }
         public int Precio { get => precio; set => precio = value; }
         public string Descripcion { get => descripcion; set => descripcion = value; }
-        public int IdDDocumento { get => idDDocumento; set => idDDocumento = value; }
+
         public int IdProducto { get => idProducto; set => idProducto = value; }
     }
 }
