f0062d5b59edca80e5f00bd5cb671a207ff57c82
Henrry <henrry5dav@outlook.com>
2020-11-18 21:38:53 -0300

    Corrección de errores

--- capaEntidades/Producto.cs
+++ capaEntidades/Producto.cs
@@ -29,7 +29,6 @@ namespace CapaEntidades
         public int Stock { get => stock; set => stock = value; }
         public int Precio { get => precio; set => precio = value; }
         public string Descripcion { get => descripcion; set => descripcion = value; }
-        public int IdDDocumento { get => idDDocumento; set => idDDocumento = value; }
         public int IdProducto { get => idProducto; set => idProducto = value; }
     }
 }
